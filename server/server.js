52da3266aaa12376472f994fd2638d13efde51b3
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-11-09 11:06:44 +0530

    express setup

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-   res.status(200).send('hey this is divyanshu tailor')
+   res.status(200).send('hey this is divyanshu ')
 })
 
 
 app.get('/register',(req,res)=>{
     res.status(200).send('Weloconme to my register page')
  })
-const PORT= 5000;
+const PORT= 7000;
 app.listen(PORT , ()=>{
     console.log(`server is start ${PORT}`)
 })
